d525279c09b7cf0d2f368b629b5db0eb8317a33a
Simon So <sso@walmartlabs.com>
2016-10-11 18:55:32 -0700

    add CI_USER_TOKEN

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo -t cocoa
 
 cd public    # Go To Public folder
+git remote set-url origin https://$CI_USER_TOKEN@github.com/sosimon/sosimon.github.io.git
 git add --all   # Add changes to git.
 
 # Commit changes.
 msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
 git commit -m "$msg"
 
 git push origin master   # Push source and build repos.
@@ -20,5 +18,5 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-cd .. 
+echo "deploy OK"
 
